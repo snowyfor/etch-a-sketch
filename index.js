daff2d7cf95318909fd2254e67e04cca246c5bae
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2024-07-14 16:45:39 +0700

    Add 'hover' effect

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ for(let i = 0; i < 16; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
+
+        square.addEventListener("mousemove", function() {
+            square.style.backgroundColor = "aquamarine";
+        });
     }
 }
