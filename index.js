c745a25c0f0707843b8f43c838497c6d81a350be
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2024-07-14 17:41:59 +0700

    Add reset button

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+// Copyright Snowyfor
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector("#reset");
 
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
+createGrid(16);
 
-        square.addEventListener("mousemove", function() {
-            square.style.backgroundColor = "aquamarine";
-        });
+resetBtn.addEventListener("click", function() {
+    let input = prompt("Grid size: (max 100)");
+    input = Number(input);
+    if (input > 0) {
+        removeGrid();
+        createGrid(input);
+    } else {
+        alert("Please enter a valid number (number > 0)");
     }
+});
+
+function createGrid(n) {
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
+            const square = document.createElement("div");
+            square.style.width = `calc(400px / ${n})`;
+            square.style.height = `calc(400px / ${n})`;
+            square.classList.add("square");
+            container.appendChild(square);
+    
+            square.addEventListener("mousemove", function() {
+                square.style.backgroundColor = "aquamarine";
+            });
+        }
+    }
+}
+
+function removeGrid() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.remove());
 }
